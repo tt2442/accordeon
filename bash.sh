5401cfb59a6f13d926a5225a4409628afd5293ce
tt2442 <theokdo@gmail.com>
2021-04-26 15:05:43 +0200

    Feat(git):mise en place

---
+++ bash.sh
@@ -0,0 +1,2 @@
+echo docker et bash ou sh  
+docker exec -i -t $1 /bin/$2
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+$config['debug']="oui";
+$config['maintenance'] = ""; //oui pour l'activer
+//ce fichier est appelé après le config.php ud framework donc il peut modifier des valeurs de celui-ci
+$config['buttontop'] = "oui"; //non pour désactiver le bouton qui permet de remonter au top
+$config['login'] = "oui"; //non pour désactiver le modal login
+
+$config['base_url'] = 'http://localhost';
